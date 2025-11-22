f4b076a2881ec2457df2870079269fdebba5c60d
Egor Geronin <egorgeronin@MacBook-Pro-Egor.local>
2025-11-22 15:10:22 +0300

    [PM-4] Сделать монорепу

---
+++ miet_practice_application/miet_practice_application/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  miet_practice_application
+//
+//  Created by Egor Geronin on 22.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ miet_practice_application/miet_practice_application/miet_practice_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  miet_practice_applicationApp.swift
+//  miet_practice_application
+//
+//  Created by Egor Geronin on 22.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct miet_practice_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
